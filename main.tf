e1cdff63e47653172c42d415726f0e34f760d920
Louie Corbo <louie.corbo@sada.com>
2022-09-09 16:09:32 -0400

    Moved out the cloud function code.

--- main.tf
+++ main.tf
@@ -9,21 +9,24 @@ resource "google_project" "smart" {
   project_id      = "lcorbo-smart-dev"
   billing_account = "01C805-AC04C5-836F50"
   folder_id       = google_folder.lcorbo.name
+}
 
+locals {
+  project_id = trimprefix(google_project.smart.id, "projects/")
 }
 
 resource "google_project_service" "cloudbuild" {
-  project = google_project.smart.id
+  project = local.project_id
   service = "cloudbuild.googleapis.com"
 }
 
 resource "google_project_service" "artifactregistry" {
-  project = google_project.smart.id
+  project = local.project_id
   service = "artifactregistry.googleapis.com"
 }
 
 resource "google_project_service" "cloudfunctions" {
-  project = google_project.smart.id
+  project = local.project_id
   service = "cloudfunctions.googleapis.com"
 }
 
@@ -32,75 +35,20 @@ resource "google_project_service" "run" {
   service = "run.googleapis.com"
 }
 
+#pubsub.googleapis.com
 
 resource "google_storage_bucket" "bucket" {
-  name                        = "lcorbo-smart-test-090822"
-  project                     = "lcorbo-smart-dev"
+  name                        = "${local.project_id}-090822"
+  project                     = local.project_id
   location                    = "US"
   uniform_bucket_level_access = true
 }
 
-data "archive_file" "cloud_function_code" {
-  type             = "zip"
-  source_dir       = "${path.module}/code/helloworld/"
-  output_file_mode = "0666"
-  output_path      = "${path.module}/bin/index.zip"
+
+output "project_id" { 
+  value = local.project_id
 }
 
-resource "google_storage_bucket_object" "archive" {
-  name   = "index.zip"
-  bucket = google_storage_bucket.bucket.name
-  source = "${path.module}/bin/index.zip"
+output "bucket" { 
+  value = google_storage_bucket.bucket.name
 }
-
-/* resource "google_cloudfunctions_function" "function" {
-  region                = "us-central1"
-  project               = google_project.smart.id
-  name                  = "nodejs-http-function"
-  description           = "nodejs-http-function"
-  runtime               = "nodejs16"
-  ingress_settings      = "ALLOW_ALL"
-  trigger_http          = true
-  source_archive_bucket = google_storage_bucket.bucket.name
-  source_archive_object = google_storage_bucket_object.archive.name
-  entry_point           = "helloGET"
-}
-
-# IAM entry for all users to invoke the function
-resource "google_cloudfunctions_function_iam_member" "invoker" {
-  project        = google_cloudfunctions_function.function.project
-  region         = google_cloudfunctions_function.function.region
-  cloud_function = google_cloudfunctions_function.function.name
-  role           = "roles/cloudfunctions.invoker"
-  member         = "allUsers"
-}  */
-
-resource "google_cloudfunctions2_function" "function" {
-  project               = "lcorbo-smart-dev"
-  name = "function-v2"
-  location = "us-east1"
-  description = "a new function"
-
-  build_config {
-    runtime = "nodejs16"
-    entry_point = "helloGET"  # Set the entry point 
-    source {
-      storage_source {
-        bucket = google_storage_bucket.bucket.name
-        object = google_storage_bucket_object.archive.name
-      }
-    }
-  }
-
-  service_config {
-    max_instance_count  = 1
-    available_memory    = "256M"
-    timeout_seconds     = 60
-  }
-}
-
-output "function_uri" { 
-  value = google_cloudfunctions2_function.function.service_config[0].uri
-}
-
-#pubsub.googleapis.com
--- provider.tf
+++ provider.tf
@@ -1,2 +1,12 @@
-provider "google" {}
+
+terraform {
+  backend "gcs" {
+    bucket  = "lcorbo-smart-dev-090822"
+    prefix  = "projects"
+  }
+}
+
+provider "google" {
+    project = "lcorbo-smart-dev"
+}
 
